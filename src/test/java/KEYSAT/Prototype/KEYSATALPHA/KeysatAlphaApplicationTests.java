0b84218ab7cbd4e1f5e0874add5a91ec423c8e11
BL-Keyin <you@example.com>
2024-01-26 15:00:21 -0500

    Upload

---
+++ src/test/java/KEYSAT/Prototype/KEYSATALPHA/KeysatAlphaApplicationTests.java
@@ -0,0 +1,13 @@
+package KEYSAT.Prototype.KEYSATALPHA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeysatAlphaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
