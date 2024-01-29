07329f5a580fe3fcf3b71ac903ca2c8355bfe274
BL-Keyin <you@example.com>
2024-01-29 08:33:12 -0500

    Auth updated

--- src/main/java/KEYSAT/Auth.java
+++ src/main/java/KEYSAT/Auth.java
@@ -1,6 +1,9 @@
 package KEYSAT;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,19 +12,32 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
+
+
+
 public class Auth {
+
 
 	@Autowired
 	private UserRepository userRepository;
 
+
 	@Autowired
-	private PasswordEncoder passwordEncoder;
+	private DataSource dataSource;
+
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	
+	}
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -38,20 +54,9 @@ public class Auth {
 		return http.build();
 	}
 
-	@Bean
-	public UserDetailsService userDetailsService() {
-		return (username) -> {
-			KEYSAT.User user = userRepository.findByUsername(username);
-			if (user != null) {
-				UserDetails userDetails = User
-						.withUsername(user.getUsername())
-						.password(user.getPassword())
-						.roles(user.getRoles().stream().map(Role::getName).toArray(String[]::new))
-						.build();
-				return userDetails;
-			} else {
-				throw new UsernameNotFoundException("User not found: " + username);
-			}
-		};
-	}
+@Bean
+public UserDetailsService userDetailsService() {
+    JdbcUserDetailsManager manager = new JdbcUserDetailsManager(dataSource);
+    return manager;
+}
 }
