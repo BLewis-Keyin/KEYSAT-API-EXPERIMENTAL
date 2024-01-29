21bf8afc47235c99e2f071f3411f7fb79594f9a1
BL-Keyin <you@example.com>
2024-01-29 07:28:31 -0500

    Experimental MySQL features

--- src/main/java/KEYSAT/Auth.java
+++ src/main/java/KEYSAT/Auth.java
@@ -1,18 +1,27 @@
 package KEYSAT;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class Auth {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -31,12 +40,18 @@ public class Auth {
 
 	@Bean
 	public UserDetailsService userDetailsService() {
-		UserDetails user = User.withDefaultPasswordEncoder()
-				.username("user")
-				.password("password")
-				.roles("USER")
-				.build();
-
-		return new InMemoryUserDetailsManager(user);
+		return (username) -> {
+			KEYSAT.User user = userRepository.findByUsername(username);
+			if (user != null) {
+				UserDetails userDetails = User
+						.withUsername(user.getUsername())
+						.password(user.getPassword())
+						.roles(user.getRoles().stream().map(Role::getName).toArray(String[]::new))
+						.build();
+				return userDetails;
+			} else {
+				throw new UsernameNotFoundException("User not found: " + username);
+			}
+		};
 	}
 }
